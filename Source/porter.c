17f0f1c393920c5e2ddbbdd2d0c3371b0bf077fb
Vladyslav Novytskyi <48761198+VNovytskyi@users.noreply.github.com>
2021-08-31 15:52:01 +0300

    Static matrix
    Dynamic not work

--- Source/porter.c
+++ Source/porter.c
@@ -1,1 +1,41 @@
 #include "porter.h"
+
+/*
+uint8_t porter_init(porter_t *desc, uint8_t package_length, uint8_t buff_length) //dynamic
+{
+    if (desc != NULL) {
+        desc->buff_length = buff_length;
+        desc->package_length = package_length;
+
+        desc->tx_buff = (uint8_t**)malloc(buff_length);
+        if (desc->tx_buff != NULL) {
+            for (uint8_t i = 0; i < buff_length; ++i) {
+                desc->tx_buff[i] = (uint8_t*)malloc(package_length);
+                if (desc->tx_buff[i] == NULL) {
+                    return PORTER_ERROR;
+                }
+                for (uint8_t j = 0; j < package_length; ++j) {
+                    desc->tx_buff[i][j] = i;
+                }
+            }
+        } else {
+            return PORTER_ERROR;
+        }
+    }
+
+    return PORTER_OK;
+}
+*/
+
+uint8_t porter_init(porter_t *desc)
+{
+    if (desc != NULL) {
+        for (uint8_t i = 0; i < PORTER_BUFFER_LENGTH; ++i) {
+            for (uint8_t j = 0; j < PORTER_PACKAGE_LENGTH; ++j) {
+                desc->tx_buff[i][j] = i;
+            }
+        }
+    }
+
+    return PORTER_OK;
+}
--- Source/porter.h
+++ Source/porter.h
@@ -1,7 +1,29 @@
 #ifndef PORTER_H
 #define PORTER_H
 
+#ifdef STM8S103
+    #include "stm8s.h"
+#endif
 
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
+#include "assert.h"
+
+#define PORTER_OK 1
+#define PORTER_ERROR 0
+
+#define PORTER_PACKAGE_LENGTH 11
+#define PORTER_BUFFER_LENGTH 12
+
+typedef struct {
+    uint8_t tx_buff[PORTER_BUFFER_LENGTH][PORTER_PACKAGE_LENGTH];
+} porter_t;
+
+//uint8_t porter_init(porter_t *desc, uint8_t package_length, uint8_t buff_length); //dynamic
+
+uint8_t porter_init(porter_t *desc);
 
 #endif // PORTER_H
