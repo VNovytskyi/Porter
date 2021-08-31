9d8de826a6d10af5490a3661e9ffeff5ae2adec2
Vladyslav Novytskyi <48761198+VNovytskyi@users.noreply.github.com>
2021-08-31 22:42:16 +0300

    Summary

--- Source/porter.c
+++ Source/porter.c
@@ -1,85 +1,55 @@
 #include "porter.h"
 
-uint8_t porter_init(porter_t *porter, const uint8_t package_length, const uint8_t buff_length) //dynamic
+uint8_t porter_init(porter_t *porter, uint8_t data_length, void (*send_callback)(uint8_t *, uint8_t), porter_time_t timeout_ms)
 {
-    if (porter != NULL) {
-        if (package_length > 0 && buff_length > 0) {
-            porter->buff_length = buff_length;
-            porter->package_length = package_length;
+    if (porter == NULL)
+        return PORTER_NULL_DESCRIPTOR;
 
-            porter->tx_buff = (uint8_t**)malloc(buff_length * sizeof(uint8_t*));
-            if (porter->tx_buff == NULL) {
-                return PORTER_ERROR;
-            }
-
-            for (uint8_t i = 0; i < buff_length; ++i) {
-                porter->tx_buff[i] = (uint8_t*)malloc(package_length * sizeof(uint8_t));
-
-                if (porter->tx_buff[i] == NULL) {
-                    return PORTER_ERROR;
-                }
-
-                for (uint8_t j = 0; j < package_length; ++j) {
-                    porter->tx_buff[i][j] = i;
-                }
-            }
-
-            porter->rx_buff = (uint8_t**)malloc(buff_length * sizeof(uint8_t*));
-            if (porter->rx_buff == NULL) {
-                return PORTER_ERROR;
-            }
-
-            for (uint8_t i = 0; i < buff_length; ++i) {
-                porter->rx_buff[i] = (uint8_t*)malloc(package_length * sizeof(uint8_t));
-
-                if (porter->rx_buff[i] == NULL) {
-                    return PORTER_ERROR;
-                }
-
-                for (uint8_t j = 0; j < package_length; ++j) {
-                    porter->rx_buff[i][j] = i;
-                }
-            }
-
-            return PORTER_OK;
-        }
+    porter->tx_buff_length = (data_length + PORTER_SERVICE_BYTES_COUNT);
+    porter->tx_buff = (uint8_t*)malloc(porter->tx_buff_length * sizeof(uint8_t));
+    if (porter->tx_buff == NULL) {
+        return PORTER_MEMORY_ERROR;
     }
 
-    return PORTER_ERROR;
+    porter->tx_empty = 1;
+    porter->send_callback = send_callback;
+    porter->timeout_ms = timeout_ms;
+
+    return PORTER_OK;
 }
 
-uint8_t porter_deinit(porter_t *porter)
+uint8_t porter_send(porter_t *porter, uint8_t *data, uint8_t data_length)
 {
-    if (porter != NULL) {
-        if (porter->tx_buff != NULL) {
-            for (uint8_t i = 0; i < porter->buff_length; ++i) {
-                free(porter->tx_buff[i]);
-            }
-            free(porter->tx_buff);
-        }
+    if (porter == NULL)
+        return PORTER_NULL_DESCRIPTOR;
 
-        if (porter->rx_buff != NULL) {
-            for (uint8_t i = 0; i < porter->buff_length; ++i) {
-                free(porter->rx_buff[i]);
-            }
-            free(porter->rx_buff);
-        }
+    if (porter->tx_empty == 0)
+        return PORTER_TX_OVERFLOW;
 
-        return PORTER_OK;
-    }
+    //Copy data to tx_buff[1]! tx_buff[0] = packType
 
-    return PORTER_ERROR;
+    return PORTER_OK;
 }
 
-uint8_t porter_write(porter_t *porter, const uint8_t *data, const uint8_t length)
+uint8_t porter_process(porter_t *porter, uint8_t *received_data, uint8_t received_data_length, porter_time_t current_time)
 {
-    if (porter != NULL) {
-        if (data != NULL && length > 0) {
-            if (length <= porter->package_length) {
-                //влезит в одну ячейку
-            } else {
-                //нужно разбивать по ячейкам
+    if (porter == NULL)
+        return PORTER_NULL_DESCRIPTOR;
+
+    //Handle RX
+    if ((received_data != NULL) && (received_data_length > 0)) {
+
+    }
+
+    //Handle TX
+    if (porter->tx_empty == 0) {
+        if ((current_time - porter->send_time) >= porter->timeout_ms) {
+            porter->send_time = current_time;
+            if (porter->send_callback != NULL) {
+                porter->send_callback(porter->tx_buff, porter->tx_buff_length);
             }
         }
     }
+
+    return PORTER_OK;
 }
--- Source/porter.h
+++ Source/porter.h
@@ -10,27 +10,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define PORTER_OK 1
-#define PORTER_ERROR 0
+#define PORTER_SERVICE_BYTES_COUNT 1
+
+#define PORTER_OK 0
+
+#define PORTER_NULL_DESCRIPTOR  10
+#define PORTER_TX_OVERFLOW      11
+#define PORTER_MEMORY_ERROR     12
+
+typedef  uint32_t porter_time_t;
 
 typedef struct {
-    uint8_t **tx_buff;
-    uint8_t tx_buff_begin;
-    uint8_t tx_buff_end;
-
-    uint8_t **rx_buff;
-    uint8_t rx_buff_begin;
-    uint8_t rx_buff_end;
+    uint8_t *tx_buff;
+    uint8_t tx_empty;
+    uint8_t tx_buff_length;
 
 
-    uint8_t package_length;
-    uint8_t buff_length;
+    porter_time_t send_time;
+    porter_time_t timeout_ms;
+
+    void (*send_callback)(uint8_t *data, uint8_t data_length);
 
 } porter_t;
 
-uint8_t porter_init(porter_t *porter, const uint8_t package_length, const uint8_t buff_length);
-uint8_t porter_deinit(porter_t *porter);
+uint8_t porter_init(porter_t *porter, uint8_t data_length, void (*send_callback)(uint8_t *data, uint8_t data_length), porter_time_t timeout_ms);
+uint8_t porter_send(porter_t *porter, uint8_t *data, uint8_t data_length);
+uint8_t porter_process(porter_t *porter, uint8_t *received_data, uint8_t received_data_length, porter_time_t current_time);
 
-uint8_t porter_write(porter_t *porter, const uint8_t *data, const uint8_t length);
 
 #endif // PORTER_H
