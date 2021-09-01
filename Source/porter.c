712c1ea35adc4245c69c4eca2ece8785fea06a02
Vladyslav Novytskyi <48761198+VNovytskyi@users.noreply.github.com>
2021-09-01 10:55:13 +0300

    Add tx free callback

--- Source/porter.c
+++ Source/porter.c
@@ -7,15 +7,15 @@ uint8_t porter_init(porter_t *porter,
                     porter_time_t timeout_ms)
 {
     if (porter == NULL)
-        return PORTER_NULL_DESCRIPTOR;
+        return PORTER_ERROR;
 
-    porter->tx_buff_max_length = (max_data_length + PORTER_SERVICE_BYTES_COUNT);
+    porter->tx_buff_max_length = (max_data_length + 1);
     porter->tx_buff = (uint8_t*)malloc(porter->tx_buff_max_length * sizeof(uint8_t));
     if (porter->tx_buff == NULL) {
-        return PORTER_MEMORY_ERROR;
+        return PORTER_ERROR;
     }
 
-    porter->tx_empty = 1;
+    porter->tx_status = PORTER_TX_FREE;
     porter->timeout_ms = timeout_ms;
     porter->send_callback = send_callback;
     porter->recv_callback = recv_callback;
@@ -25,23 +25,32 @@ uint8_t porter_init(porter_t *porter,
     return PORTER_OK;
 }
 
+uint8_t porter_deinit(porter_t *porter)
+{
+    if (porter == NULL)
+        return PORTER_ERROR;
+
+    free(porter->tx_buff);
+    return PORTER_OK;
+}
+
 uint8_t porter_send(porter_t *porter, uint8_t *data, uint8_t data_length)
 {
     if (porter == NULL)
-        return PORTER_NULL_DESCRIPTOR;
+        return PORTER_ERROR;
 
-    if (porter->tx_empty == 0)
-        return PORTER_TX_OVERFLOW;
+    if (porter->tx_status == PORTER_TX_BUSY)
+        return PORTER_ERROR;
 
     if (data == NULL)
-        return PORTER_NULL_DATA;
+        return PORTER_ERROR;
 
     if (data_length > (porter->tx_buff_max_length - 1))
-        return PORTER_LONG_DATA;
+        return PORTER_ERROR;
 
-    porter->tx_empty = 0;
+    porter->tx_status = PORTER_TX_BUSY;
     porter->tx_buff[0] = PORTER_DATA_PACK_ID;
-    porter->current_message_length = data_length + 1;
+    porter->current_tx_data_length = data_length + 1;
     memcpy((porter->tx_buff + 1), data, data_length);
 
     return PORTER_OK;
@@ -50,31 +59,51 @@ uint8_t porter_send(porter_t *porter, uint8_t *data, uint8_t data_length)
 uint8_t porter_process(porter_t *porter, uint8_t *received_data, uint8_t received_data_length, porter_time_t current_time)
 {
     if (porter == NULL)
-        return PORTER_NULL_DESCRIPTOR;
+        return PORTER_ERROR;
 
     //Handle RX
     if ((received_data != NULL) && (received_data_length > 0)) {
         if (received_data[0] == PORTER_ACK_PACK_ID) {
-            porter->tx_empty = 1;
+            porter->tx_status = PORTER_TX_FREE;
+            if (porter->tx_free_callback != NULL) { // написать макрос
+                porter->tx_free_callback();
+            }
         } else if (received_data[1] == PORTER_DATA_PACK_ID) {
-            if (porter->send_callback != NULL) {
+            if (porter->send_callback != NULL) {// написать макрос
                 porter->send_callback(porter->ack, 1);
             }
-            if (porter->recv_callback != NULL) {
+            if (porter->recv_callback != NULL) {// написать макрос
                 porter->recv_callback((received_data + 1), (received_data_length - 1));
             }
         }
     }
 
     //Handle TX
-    if (porter->tx_empty == 0) {
+    if (porter->tx_status == PORTER_TX_BUSY) {
         if ((current_time - porter->send_time) >= porter->timeout_ms) {
             porter->send_time = current_time;
-            if (porter->send_callback != NULL) {
-                porter->send_callback(porter->tx_buff, porter->current_message_length);
+            if (porter->send_callback != NULL) {// написать макрос
+                porter->send_callback(porter->tx_buff, porter->current_tx_data_length);
             }
         }
     }
 
+    return PORTER_OK;
+}
+
+uint8_t porter_is_tx_free(porter_t *porter)
+{
+    if (porter == NULL)
+        return PORTER_ERROR;
+
+    return porter->tx_status;
+}
+
+uint8_t porter_set_tx_free_callback(porter_t *porter, void (*tx_free_callback)())
+{
+    if (porter == NULL)
+        return PORTER_ERROR;
+
+    porter->tx_free_callback = tx_free_callback;
     return PORTER_OK;
 }
--- Source/porter.h
+++ Source/porter.h
@@ -10,32 +10,31 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define PORTER_ACK_PACK_ID 2
 #define PORTER_DATA_PACK_ID 1
+#define PORTER_ACK_PACK_ID  2
 
-#define PORTER_SERVICE_BYTES_COUNT 1
-
-#define PORTER_OK 0
-#define PORTER_NULL_DESCRIPTOR  10
-#define PORTER_TX_OVERFLOW      11
-#define PORTER_MEMORY_ERROR     12
-#define PORTER_LONG_DATA        13
-#define PORTER_NULL_DATA        14
+#define PORTER_OK       0
+#define PORTER_ERROR    1
+#define PORTER_TX_BUSY  2
+#define PORTER_TX_FREE  3
 
 typedef  uint32_t porter_time_t;
 
 typedef struct {
     uint8_t *tx_buff;
     uint8_t tx_buff_max_length;
-    uint8_t tx_empty;
-    uint8_t current_message_length;
+
+    uint8_t tx_status;
+    uint8_t current_tx_data_length;
 
     uint8_t ack[1];
 
     porter_time_t send_time;
     porter_time_t timeout_ms;
+
     void (*send_callback)(uint8_t *data, uint8_t data_length);
     void (*recv_callback)(uint8_t *data, uint8_t data_length);
+    void (*tx_free_callback)();
 
 } porter_t;
 
@@ -44,6 +43,11 @@ uint8_t porter_init(porter_t *porter,
                     void (*send_callback)(uint8_t *data, uint8_t data_length),
                     void (*recv_callback)(uint8_t *data, uint8_t data_length),
                     porter_time_t timeout_ms);
+
+uint8_t porter_deinit(porter_t *porter);
+
+uint8_t porter_is_tx_free(porter_t *porter);
+uint8_t porter_set_tx_free_callback(porter_t *porter, void (*tx_free_callback)());
 
 uint8_t porter_send(porter_t *porter, uint8_t *data, uint8_t data_length);
 uint8_t porter_process(porter_t *porter, uint8_t *received_data, uint8_t received_data_length, porter_time_t current_time);
