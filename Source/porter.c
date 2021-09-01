317a8ecd585ff2f257cd9f61a6e4982777faf342
Vladyslav Novytskyi <48761198+VNovytskyi@users.noreply.github.com>
2021-09-01 10:25:58 +0300

    Ready to test

--- Source/porter.c
+++ Source/porter.c
@@ -1,19 +1,26 @@
 #include "porter.h"
 
-uint8_t porter_init(porter_t *porter, uint8_t data_length, void (*send_callback)(uint8_t *, uint8_t), porter_time_t timeout_ms)
+uint8_t porter_init(porter_t *porter,
+                    uint8_t max_data_length,
+                    void (*send_callback)(uint8_t *, uint8_t),
+                    void (*recv_callback)(uint8_t *, uint8_t),
+                    porter_time_t timeout_ms)
 {
     if (porter == NULL)
         return PORTER_NULL_DESCRIPTOR;
 
-    porter->tx_buff_length = (data_length + PORTER_SERVICE_BYTES_COUNT);
-    porter->tx_buff = (uint8_t*)malloc(porter->tx_buff_length * sizeof(uint8_t));
+    porter->tx_buff_max_length = (max_data_length + PORTER_SERVICE_BYTES_COUNT);
+    porter->tx_buff = (uint8_t*)malloc(porter->tx_buff_max_length * sizeof(uint8_t));
     if (porter->tx_buff == NULL) {
         return PORTER_MEMORY_ERROR;
     }
 
     porter->tx_empty = 1;
-    porter->send_callback = send_callback;
     porter->timeout_ms = timeout_ms;
+    porter->send_callback = send_callback;
+    porter->recv_callback = recv_callback;
+
+    porter->ack[0] = PORTER_ACK_PACK_ID;
 
     return PORTER_OK;
 }
@@ -26,7 +33,16 @@ uint8_t porter_send(porter_t *porter, uint8_t *data, uint8_t data_length)
     if (porter->tx_empty == 0)
         return PORTER_TX_OVERFLOW;
 
-    //Copy data to tx_buff[1]! tx_buff[0] = packType
+    if (data == NULL)
+        return PORTER_NULL_DATA;
+
+    if (data_length > (porter->tx_buff_max_length - 1))
+        return PORTER_LONG_DATA;
+
+    porter->tx_empty = 0;
+    porter->tx_buff[0] = PORTER_DATA_PACK_ID;
+    porter->current_message_length = data_length + 1;
+    memcpy((porter->tx_buff + 1), data, data_length);
 
     return PORTER_OK;
 }
@@ -38,7 +54,16 @@ uint8_t porter_process(porter_t *porter, uint8_t *received_data, uint8_t receive
 
     //Handle RX
     if ((received_data != NULL) && (received_data_length > 0)) {
-
+        if (received_data[0] == PORTER_ACK_PACK_ID) {
+            porter->tx_empty = 1;
+        } else if (received_data[1] == PORTER_DATA_PACK_ID) {
+            if (porter->send_callback != NULL) {
+                porter->send_callback(porter->ack, 1);
+            }
+            if (porter->recv_callback != NULL) {
+                porter->recv_callback((received_data + 1), (received_data_length - 1));
+            }
+        }
     }
 
     //Handle TX
@@ -46,7 +71,7 @@ uint8_t porter_process(porter_t *porter, uint8_t *received_data, uint8_t receive
         if ((current_time - porter->send_time) >= porter->timeout_ms) {
             porter->send_time = current_time;
             if (porter->send_callback != NULL) {
-                porter->send_callback(porter->tx_buff, porter->tx_buff_length);
+                porter->send_callback(porter->tx_buff, porter->current_message_length);
             }
         }
     }
--- Source/porter.h
+++ Source/porter.h
@@ -10,30 +10,41 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define PORTER_ACK_PACK_ID 2
+#define PORTER_DATA_PACK_ID 1
+
 #define PORTER_SERVICE_BYTES_COUNT 1
 
 #define PORTER_OK 0
-
 #define PORTER_NULL_DESCRIPTOR  10
 #define PORTER_TX_OVERFLOW      11
 #define PORTER_MEMORY_ERROR     12
+#define PORTER_LONG_DATA        13
+#define PORTER_NULL_DATA        14
 
 typedef  uint32_t porter_time_t;
 
 typedef struct {
     uint8_t *tx_buff;
+    uint8_t tx_buff_max_length;
     uint8_t tx_empty;
-    uint8_t tx_buff_length;
+    uint8_t current_message_length;
 
+    uint8_t ack[1];
 
     porter_time_t send_time;
     porter_time_t timeout_ms;
-
     void (*send_callback)(uint8_t *data, uint8_t data_length);
+    void (*recv_callback)(uint8_t *data, uint8_t data_length);
 
 } porter_t;
 
-uint8_t porter_init(porter_t *porter, uint8_t data_length, void (*send_callback)(uint8_t *data, uint8_t data_length), porter_time_t timeout_ms);
+uint8_t porter_init(porter_t *porter,
+                    uint8_t max_data_length,
+                    void (*send_callback)(uint8_t *data, uint8_t data_length),
+                    void (*recv_callback)(uint8_t *data, uint8_t data_length),
+                    porter_time_t timeout_ms);
+
 uint8_t porter_send(porter_t *porter, uint8_t *data, uint8_t data_length);
 uint8_t porter_process(porter_t *porter, uint8_t *received_data, uint8_t received_data_length, porter_time_t current_time);
 
